2bfaadd54749473dd6af51ca1d7b737c58b8ad3e
Gabgasa <32471189+Gabgasa@users.noreply.github.com>
2019-01-26 13:15:26 -0200

    Movimento simples e comeco do karma

---
+++ Project Hewenjerz/Assets/Scripts/NPC.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPC : MonoBehaviour
+{
+    public bool arranhado = false;
+    public bool interagido = false;
+    public bool acariciado = false;
+    public int variacaoKarma;
+
+    public int karma = 50;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(arranhado == true)
+        {
+            karma = karma - variacaoKarma;
+            arranhado = false;
+        }
+
+        if (acariciado == true)
+        {
+            karma = karma + variacaoKarma;
+            acariciado = false;
+        }
+    }
+
+
+}
---
+++ Project Hewenjerz/Assets/Scripts/movimento.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movimento : MonoBehaviour
+{
+
+
+    public float velocidadeX = 10f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //DIREITA
+        if (Input.GetKey("d"))
+        {
+            //Debug.Log("aaaaaa");
+            transform.Translate((velocidadeX * Time.deltaTime), 0, 0);
+        }
+
+        //ESQUERDA
+        if (Input.GetKey("a"))
+        {
+            transform.Translate((-velocidadeX * Time.deltaTime), 0, 0);
+        }
+    }
+}
