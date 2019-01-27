b5e0c0e36661d3bad7d8edad47cb81f58f871c6d
Trigs <mtrigsauer@gmail.com>
2019-01-27 14:12:04 -0200

    Credits and gamemanager changes

--- Project Hewenjerz/Assets/Scripts/GameManager.cs
+++ Project Hewenjerz/Assets/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -97,6 +98,7 @@ public class GameManager : MonoBehaviour
             if(dia >= fim) 
             {
 				//camerAnim.SetBool ("Clear", true);
+				SceneManager.LoadScene("Credits");
                 Debug.Log("FIM");
             }
         }
